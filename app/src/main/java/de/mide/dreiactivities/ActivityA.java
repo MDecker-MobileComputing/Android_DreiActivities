7ecec7cd3b5d3d65d3c501a39c2faeb1d5fae1e5
mide42 <demi_05@arcor.de>
2019-05-25 21:01:14 +0200

    Upgrade für Android Studo 3.4

--- app/src/main/java/de/mide/dreiactivities/ActivityA.java
+++ app/src/main/java/de/mide/dreiactivities/ActivityA.java
@@ -22,6 +22,7 @@ public class ActivityA extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
     }
--- app/src/main/java/de/mide/dreiactivities/ActivityB.java
+++ app/src/main/java/de/mide/dreiactivities/ActivityB.java
@@ -21,6 +21,7 @@ public class ActivityB extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
     }
